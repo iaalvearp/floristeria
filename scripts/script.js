66a0f2b930245005ece7f652464a69114ab75f0c
user <iaalvearp@gmail.com>
2022-05-08 23:13:22 -0500

    last corrections

--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,12 @@ const links_menu = document.getElementById('nav')
 
 const show_menu = () => {
     links_menu.style.top = '0';
+    btn_Menu.style.display = 'none'
 };
 
 const hide_menu = () => {
     links_menu.style.top = '-70rem';
+    btn_Menu.style.display = 'block'
 }
 
 document.getElementById('nav__mobile_img').addEventListener('click', show_menu);
